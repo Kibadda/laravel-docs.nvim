03edae643a278e0d5cfd20e389f141546bad4c01
Michael Strobel <mstrobel97@gmail.com>
2022-04-14 21:38:30 +0200

    directory restructure

--- lua/telescope/_extensions/laravel-docs.lua
+++ lua/telescope/_extensions/laravel-docs.lua
@@ -1,39 +1,13 @@
-local pickers = require 'telescope.pickers'
-local finders = require 'telescope.finders'
-local conf = require('telescope.config').values
-local actions = require 'telescope.actions'
-local action_state = require 'telescope.actions.state'
 local has_telescope, telescope = pcall(require, 'telescope')
+local main = require('telescope._extensions.laravel-docs.main')
 
 if not has_telescope then
   error('This plugins requires nvim-telescope/telescope.nvim')
 end
 
-local docnames = require('telescope._extensions.laravel-docs.docs')
-
-local baseurl = 'https://laravel.com/docs/'
-
-local docs = function (opts)
-  opts = opts or {}
-  pickers.new(opts, {
-    prompt_title = 'Laravel Documentation',
-    finder = finders.new_table {
-      results = docnames
-    },
-    sorter = conf.generic_sorter(opts),
-    attach_mappings = function(prompt_bufnr)
-      actions.select_default:replace(function()
-        actions.close(prompt_bufnr)
-        local selection = action_state.get_selected_entry()
-        os.execute('xdg-open ' .. baseurl .. selection[1])
-      end)
-      return true
-    end,
-  }):find()
-end
-
 return telescope.register_extension {
+  setup = main.setup,
   exports = {
-    laraveldocs = docs,
+    laraveldocs = main.laraveldocs,
   },
 }
---
+++ lua/telescope/_extensions/laravel-docs/main.lua
@@ -0,0 +1,50 @@
+-- telescope modules
+local pickers = require 'telescope.pickers'
+local finders = require 'telescope.finders'
+local conf = require('telescope.config').values
+local actions = require 'telescope.actions'
+local action_state = require 'telescope.actions.state'
+
+-- laravel docs modules
+local docnames = require('telescope._extensions.laravel-docs.docs')
+
+local baseurl = 'https://laravel.com/docs/'
+
+local M = {}
+
+local version
+
+M.setup = function (config)
+  version = config.version or nil
+end
+
+M.laraveldocs = function (opts)
+  opts = opts or {}
+  pickers.new(opts, {
+    prompt_title = 'Laravel Documentation',
+    results_title = 'Sites',
+    finder = finders.new_table {
+      results = docnames
+    },
+    sorter = conf.generic_sorter(opts),
+    attach_mappings = function()
+      actions.select_default:replace(function()
+        local url = baseurl
+        if version ~= nil then
+          url = url .. '/' .. version
+        end
+
+        local open_docs = function ()
+          local selection = action_state.get_selected_entry()
+          os.execute('xdg-open ' .. url .. '/' .. selection[1])
+        end
+
+        actions.select_default:replace(open_docs)
+      end)
+      return true
+    end,
+  }):find()
+end
+
+return M
+
