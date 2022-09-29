1a0929429d017df114057ef4a59254b37cd03fb2
Michael Strobel <mstrobel97@gmail.com>
2022-09-29 14:18:14 +0200

    add checks for telescope

--- lua/telescope/_extensions/laravel-docs.lua
+++ lua/telescope/_extensions/laravel-docs.lua
@@ -1,6 +1,15 @@
-return require("telescope").register_extension {
-  setup = require("laravel-docs").setup,
+local ok, telescope = pcall(require, "telescope")
+
+if not ok then
+  error "This plugin needs telescope installed"
+end
+
+local laravel_docs = require "laravel-docs"
+local picker = require "laravel-docs.picker"
+
+return telescope.register_extension {
+  setup = laravel_docs.setup,
   exports = {
-    ["laravel-docs"] = require("laravel-docs.telescope").laravel_docs,
+    ["laravel-docs"] = picker.laravel_docs,
   },
 }
