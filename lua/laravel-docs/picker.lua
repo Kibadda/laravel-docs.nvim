a211536d03a7b245c88ca60c81d091ffd85ee80b
Michael Strobel <mstrobel97@gmail.com>
2022-10-07 14:37:07 +0200

    new finder/picker for all headings of a doc site

--- lua/laravel-docs/picker.lua
+++ lua/laravel-docs/picker.lua
@@ -25,9 +25,85 @@ function M.laravel_docs(opts)
       results = results,
       entry_maker = function(entry)
         return {
-          value = entry.path,
+          path = entry.path,
           display = entry.name,
           ordinal = entry.name,
+          entry = entry,
+        }
+      end,
+    },
+    sorter = conf.generic_sorter(opts),
+    mappings = {
+      i = {
+        ["<C-o>"] = false,
+      },
+    },
+    attach_mappings = function(prompt_bufnr, map)
+      actions.select_default:replace(function()
+        opts.doc_site = action_state.get_selected_entry().entry
+        M.sub_heading(opts)
+      end)
+
+      local function open_doc_site()
+        local selection = action_state.get_selected_entry().entry
+        actions.close(prompt_bufnr)
+
+        local url = base_url
+        if version then
+          url = url .. version .. "/"
+        end
+        Job:new({
+          command = "xdg-open",
+          args = {
+            url .. selection.slug,
+          },
+        }):start()
+      end
+
+      map("i", "<C-o>", open_doc_site)
+
+      map("n", "o", open_doc_site)
+
+      return true
+    end,
+  }
+
+  if laravel_docs.opts.preview then
+    if laravel_docs.opts.preview_with_glow and vim.fn.executable "glow" == 1 then
+      picker_opts.previewer = previewers.new_termopen_previewer {
+        get_command = function(entry)
+          return {
+            "glow",
+            "-p",
+            entry.path,
+          }
+        end,
+      }
+    else
+      picker_opts.previewer = conf.file_previewer {}
+    end
+  end
+
+  pickers.new(opts, picker_opts):find()
+end
+
+function M.sub_heading(opts)
+  opts = opts or {}
+
+  local version = laravel_docs.opts.version
+
+  local results = laravel_docs_finders.find_sub_headings(opts.doc_site.slug)
+
+  local picker_opts = {
+    prompt_title = "Laravel Documentation " .. opts.doc_site.name,
+    finder = finders.new_table {
+      results = results,
+      entry_maker = function(entry)
+        return {
+          path = opts.doc_site.path,
+          display = entry.name,
+          ordinal = entry.lnum,
+          lnum = entry.lnum,
           entry = entry,
         }
       end,
@@ -45,7 +121,7 @@ function M.laravel_docs(opts)
         Job:new({
           command = "xdg-open",
           args = {
-            url .. selection.slug,
+            url .. opts.doc_site.slug .. "#" .. selection.anchor,
           },
         }):start()
       end)
@@ -54,19 +130,7 @@ function M.laravel_docs(opts)
   }
 
   if laravel_docs.opts.preview then
-    if laravel_docs.opts.preview_with_glow and vim.fn.executable "glow" == 1 then
-      picker_opts.previewer = previewers.new_termopen_previewer {
-        get_command = function(entry)
-          return {
-            "glow",
-            "-p",
-            entry.value,
-          }
-        end,
-      }
-    else
-      picker_opts.previewer = conf.file_previewer {}
-    end
+    picker_opts.previewer = conf.grep_previewer {}
   end
 
   pickers.new(opts, picker_opts):find()
